1cfb4f3f78b807776d764a2242b3fdb3cadd0900
arpapon <ashfaqurrahman377@gmail.com>
2024-06-21 01:30:45 +0600

    lasest commett

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import router from './app/routes';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
+import httpStatus from 'http-status';
 // import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
@@ -10,6 +11,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', router);
+
+app.get('/', (req, res) => {
+  res.status(httpStatus.OK).send('Well come to car rental service');
+});
 
 app.use(globalErrorHandler);
 
